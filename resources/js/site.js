b23052b332c273214f637176376f4e8cb632e8c3
fmeyer1980 <fmeyer01@hotmail.com>
2022-11-25 17:00:51 +0100

    ret

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,11 +1,17 @@
 import Alpine from 'alpinejs'
 import intersect from '@alpinejs/intersect'
- 
+import Swup from 'swup';
+import SwupPreloadPlugin from '@swup/preload-plugin';
+
 Alpine.plugin(intersect)
 
 window.Alpine = Alpine
  
 Alpine.start()
 
-import Turbolinks from 'turbolinks';
-Turbolinks.start();
+const swup = new Swup({
+    plugins: [new SwupPreloadPlugin()]
+  });
+
+// import Turbolinks from 'turbolinks';
+// Turbolinks.start();
